66e06c33ac0363caa66e7f8a5c519e8d464269ec
techhomare <homare.2715@gmail.com>
2021-05-30 23:53:17 +0900

    正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
   validates :first_name , presence: true
   validates :last_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/ } do
   validates :ruby_first_name, presence: true
   validates :ruby_last_name, presence: true
+  end
   validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
